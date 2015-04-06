3d46ffaeb246c99ddda2c5abdd9f633f5fb98b47
Irene1612 <vicmatil.igf@gmail.com>
2015-04-06 13:25:38 +0200

    Hecho metodo caberedTableauCards que crea las cartas tapadas de los montones de trabajo.

--- src/StartController.java
+++ src/StartController.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-
 public class StartController {
 
 	public boolean isEmptyAllFoundations() {
@@ -16,11 +15,10 @@ public class StartController {
 		ArrayList<Card> peekEachTableau = new ArrayList<Card>();
 		for (int i = 0; i < 7; i++) {
 			Card card;
-			do{
+			do {
 				Random r = new Random();
-				card = new Card(r.nextInt(3), r.nextInt(11));
-				
-			}while(peekEachTableau.contains(card));
+				card = new Card(r.nextInt(3), r.nextInt(11), false);
+			} while (peekEachTableau.contains(card));
 			peekEachTableau.add(card);
 		}
 		return peekEachTableau;
@@ -30,5 +28,20 @@ public class StartController {
 		return true;
 	}
 
-	
+	public ArrayList<ArrayList<Card>> coveredTableauCards() {
+		ArrayList<ArrayList<Card>> coveredTableauCards = new ArrayList<ArrayList<Card>>();
+		for (int i = 0; i < 7; i++) {
+			coveredTableauCards.add(new ArrayList<Card>());
+			for (int j = 0; j < i; j++){
+				Card card;
+				do {
+					Random r = new Random();
+					card = new Card(r.nextInt(3), r.nextInt(11), true);
+				} while (coveredTableauCards.get(i).contains(card));
+				coveredTableauCards.get(i).add(card);
+			}
+		}
+		return coveredTableauCards;
+	}
+
 }
