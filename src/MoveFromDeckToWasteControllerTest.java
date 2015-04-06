7e003d3e778fd1b2be36f3bb61a0524bce769ed0
Irene1612 <vicmatil.igf@gmail.com>
2015-04-06 19:38:30 +0200

    Creado test para el movimiento que consiste en pasar las tres primeras cartas del deck al waste.

--- src/MoveFromDeckToWasteControllerTest.java
+++ src/MoveFromDeckToWasteControllerTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -24,6 +26,15 @@ public class MoveFromDeckToWasteControllerTest {
 
 	@Test
 	public void moveCardFromDeskToWasteTest() {
-
+		ArrayList<Card> cardsFromDeskToWaste = new ArrayList<Card>(3);
+		for (int i = 0; i < 3; i++) {
+			cardsFromDeskToWaste.add(moveFromDeckToWasteController.getDeskSet().get(i));
+		}
+		moveFromDeckToWasteController.moveCardFromDeskToWaste();
+		assertEquals(21, moveFromDeckToWasteController.getDeskSet().size());
+		assertEquals(3, moveFromDeckToWasteController.getWasteSet().size());
+		for (int i = 0; i < 3; i++) {
+			assertEquals(cardsFromDeskToWaste.get(i), moveFromDeckToWasteController.getWasteSet().get(i));
+		}
 	}
 }
