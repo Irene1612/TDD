dc33e640d2f763f11b1801ac75649d3399716f76
Irene1612 <vicmatil.igf@gmail.com>
2015-04-07 10:21:57 +0200

    Creado test MoveFromWasteToDeckControllerTest.

---
+++ src/MoveFromWasteToDeckControllerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+
+
+public class MoveFromWasteToDeckControllerTest {
+	
+	MoveFromWasteToDeckController moveFromWasteToDeckController;
+
+	@Before
+	public void before() {
+		moveFromWasteToDeckController = new MoveFromWasteToDeckController();
+	}
+}
