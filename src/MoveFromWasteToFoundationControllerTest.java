cbf0b5096b91904c8bf85f407e9a1016a2609fbf
Irene1612 <vicmatil.igf@gmail.com>
2015-04-07 13:25:14 +0200

    Creado test MoveFromWasteToFoundationControllerTest.

---
+++ src/MoveFromWasteToFoundationControllerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+
+
+public class MoveFromWasteToFoundationControllerTest {
+	
+	MoveFromWasteToFoundationController moveFromWasteToFoundationController;
+	
+	@Before
+	public void before() {
+		moveFromWasteToFoundationController = new MoveFromWasteToFoundationController();
+	}
+}
