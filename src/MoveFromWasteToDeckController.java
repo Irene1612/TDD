d31e0455360104fc37e0982bdc6a79462405c526
Irene1612 <vicmatil.igf@gmail.com>
2015-04-07 12:20:05 +0200

    Creado el deck en el controlador MoveFromWasteToDeckController y su inicialización. Pasa el test.

--- src/MoveFromWasteToDeckController.java
+++ src/MoveFromWasteToDeckController.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+
 
 public class MoveFromWasteToDeckController {
+	
+	ArrayList<Card> deckSet;
+	
+	public MoveFromWasteToDeckController() {
+		this.initializeDeck();
+	}
+
+	private void initializeDeck() {
+		this.deckSet = new ArrayList<Card>();
+	}
+
+	public ArrayList<Card> getDeckSet() {
+		return deckSet;
+	}
 
 }
