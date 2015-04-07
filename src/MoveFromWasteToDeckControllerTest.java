82ceb8a5b4a8fd82424c791544c108abb7634493
Irene1612 <vicmatil.igf@gmail.com>
2015-04-07 12:47:21 +0200

    Creado test para la inicializacion del waste: initializeWasteTest.

--- src/MoveFromWasteToDeckControllerTest.java
+++ src/MoveFromWasteToDeckControllerTest.java
@@ -1,20 +1,36 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class MoveFromWasteToDeckControllerTest {
-	
+
 	MoveFromWasteToDeckController moveFromWasteToDeckController;
 
 	@Before
 	public void before() {
 		moveFromWasteToDeckController = new MoveFromWasteToDeckController();
 	}
-	
+
 	@Test
 	public void initializeDeckTest() {
 		assertEquals(0, moveFromWasteToDeckController.getDeckSet().size());
+	}
+
+	@Test
+	public void initializeWasteTest() {
+		ArrayList<Card> wasteSet = moveFromWasteToDeckController.getWasteSet();
+		int wasteSize = wasteSet.size();
+		assertTrue(wasteSize > 0 && wasteSize < 25);
+		for (int i = 0; i < wasteSize; i++) {
+			Card card = wasteSet.get(i);
+			assertNotNull(card);
+			assertFalse(card.isCovered());
+			wasteSet.remove(card);
+			assertFalse(wasteSet.contains(card));
+			wasteSet.add(card);
+		}
 	}
 }
