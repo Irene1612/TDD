960d110813ab638a31b804e40649b99e65c8a678
Irene1612 <vicmatil.igf@gmail.com>
2015-04-07 12:56:36 +0200

    Creado metodo initializeWaste en el controlador MoveFromWasteToDeckController. Pasa el test.

--- src/MoveFromWasteToDeckController.java
+++ src/MoveFromWasteToDeckController.java
@@ -1,20 +1,41 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class MoveFromWasteToDeckController {
 	
 	ArrayList<Card> deckSet;
+	ArrayList<Card> wasteSet;
 	
 	public MoveFromWasteToDeckController() {
 		this.initializeDeck();
-	}
-
-	private void initializeDeck() {
-		this.deckSet = new ArrayList<Card>();
-	}
+		this.initializeWaste();
+	}	
 
 	public ArrayList<Card> getDeckSet() {
 		return deckSet;
+	}
+
+	public ArrayList<Card> getWasteSet() {
+		return wasteSet;
+	}
+	
+	private void initializeDeck() {
+		this.deckSet = new ArrayList<Card>();
+	}
+	
+	private void initializeWaste() {
+		this.wasteSet = new ArrayList<Card>();
+		Random r = new Random();
+		int wasteSize = r.nextInt(25);
+		for (int i = 0; i < wasteSize; i++) {
+			Card card;
+			do {
+				r = new Random();
+				card = new Card(r.nextInt(3), r.nextInt(11), false);
+			} while (this.wasteSet.contains(card));
+			this.wasteSet.add(card);
+		}
 	}
 
 }
